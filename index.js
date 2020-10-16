726a5a8ea9d9019480c4ea2fe6b3609f894f1c48
BH-Dla33a <70106486+BH-Dla33a@users.noreply.github.com>
2020-10-16 09:44:47 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '+';
+const { MessageEmbed } = require('discord.js');
+
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+
+
+
+  
+
+client.login('NzYyMzA5NDM5NDQ0ODc3MzM0.X3nR_Q.BRat37Z8cPatJv8cQNrBfGwuXUQ')
